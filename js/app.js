7304a25950b6709da559c230c1cb02642a189145
Hanady khrishah <hanady.awad94@yahoo.com>
2021-06-16 18:36:10 +0300

    add new store

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let parentElement = document.getElementById('salesData');
+// let parentElement = document.getElementById('salesData');
 let salesTable = document.getElementById('salesTable');
+let storeForm = document.getElementById('storeForm');
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 function CookieStand(name, min, max, avg) {
@@ -25,16 +26,16 @@ CookieStand.prototype.getCustomer = function () {
     this.total += cookies;
   }
 
-}
+};
 CookieStand.prototype.CookiesPerHour = function () {
   for (let i = 0; i < hours.length; i++) {
     let perHour = Math.ceil(this.avgCookies * this.averageCustomersPerHour[i]);
 
     this.CookiesPerHour.push(perHour);
-    this.totalCookies += perHour
+    this.totalCookies += perHour;
   }
 
-}
+};
 CookieStand.prototype.render = function () {
   let tr = document.createElement('tr');
   salesTable.appendChild(tr);
@@ -48,7 +49,7 @@ CookieStand.prototype.render = function () {
     tr.appendChild(td);
     td.textContent = this.CookiesPerHour[i];
 
-  };
+  }
   let total = document.createElement('th');
   tr.appendChild(total);
   total.textContent = this.total;
@@ -97,7 +98,7 @@ function makeTableHeader() {
     th2.textContent = hours[i];
 
   }
-  let th3 = document.createElement('th')
+  let th3 = document.createElement('th');
   tr.appendChild(th3);
   th3.textContent = 'Daily totals';
 }
@@ -133,3 +134,25 @@ function makeTableFooter() {
 function getRandomCustomer(min , max){
   return Math.floor(Math.random() * max - min) + min ;
 }
+
+function addNewCookieShop(event) {
+  event.preventDefault();
+
+  let name = event.target.StoreName.value;
+  let min = event.target.minCust.value;
+  let max = event.target.maxCust .value;
+  let avg = event.target.avgCookie.value;
+
+  let newLoc =  new CookieStand (name ,min ,max ,avg );
+
+
+
+  newLoc.getCustomer();
+  newLoc.render();
+  makeTableFooter();
+
+
+}
+
+
+storeForm.addEventListener('submit',addNewCookieShop);
